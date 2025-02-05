aff583225382a597163b5cbea84685e6ed5be7f5
Sandra <sanfurdia@alu.edu.gva.es>
2025-02-05 14:54:12 +0100

    cambios sobre marca

--- Inventario/src/app/marca/components/lista/lista.component.ts
+++ Inventario/src/app/marca/components/lista/lista.component.ts
@@ -9,13 +9,14 @@ import { MarcasService } from '../../marcas.service';
 export class ListaComponent {
   public filterSearch:string = '';
   public cargando:boolean = true;
-  marcas: any;
+  public marcas: any;
   constructor(private _marcasService: MarcasService) { }
   ngOnInit() {
     this._marcasService.obtengoMarcasApi().subscribe({
       next: (resultado) => {
         if (resultado.mensaje == "OK"){
           this.marcas = resultado.datos;
+          console.log(resultado.datos);
         }else{
           console.error('Error al recibir datos:', resultado.error);
         }
--- Inventario/src/app/marca/marcas.service.ts
+++ Inventario/src/app/marca/marcas.service.ts
@@ -7,8 +7,8 @@ import { Marca } from './marca';
   providedIn: 'root'
 })
 export class MarcasService {
-  urlPhp=''/*'http://test-php.jtarrega.es/';*/;
-  urlApi=''/*'http://test-api25s.jtarrega.es/api/empleados';*/;
+  urlPhp='https://uat-inventarios.proyectos-2daw.es/';
+  urlApi='https://uat-inventarios.proyectos-2daw.es/api/marcaControl.php';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':'application/json'
