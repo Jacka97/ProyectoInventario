c76f0901ba8c05c5c4adfac72b68fb0b41d83d1d
Desock <salvagomezrisen@gmail.com>
2025-02-06 15:54:45 +0100

    Añadido codigo en ordenadores.component.ts

--- Inventario/src/app/ordenadores/components/ordenadores/ordenadores.component.ts
+++ Inventario/src/app/ordenadores/components/ordenadores/ordenadores.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Ordenadores } from '../../ordenadores';
+import { OrdenadoresService } from '../../ordenadores.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-ordenadores',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrl: './ordenadores.component.css'
 })
 export class OrdenadoresComponent {
+  public ordenadorAct: Ordenadores = { 
+    id: 0, 
+    numero: '', 
+    idMarca: 0, 
+    modelo: '', 
+    idUbicacion: 0, 
+    nombre: '', 
+    tipo: '', 
+    numeroSerie: '', 
+    red: '', 
+    macLAN: '', 
+    macWifi: '', 
+    ipWifi: '', 
+    hd1: '', 
+    hd2: '', 
+    observaciones: '' 
+  };
+
+  constructor(private _ordenadoresService: OrdenadoresService, private _aroute: ActivatedRoute, private _route: Router, private toastr: ToastrService) {}
+
+  private traeOrdenador() {
+    this._ordenadoresService.obtengoOrdenadores().subscribe({
+      next: (resultado) => {
+        if (resultado.mensaje == "OK") {
+          this.ordenadorAct = resultado.datos;
+        } else {
+          this.toastr.error(resultado.mensaje, 'Error al obtener el ordenador');
+        }
+      },
+      error: (error) => {
+        this.toastr.error(error, 'Error al obtener el ordenador')
+      },
+      complete: () => {
+        console.log('Operacion completada');
+      },
+    });
+  }
 
 }
