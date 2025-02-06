1392acfa723978745b44d195da4910db11b9688e
Tony Abril <antabrrue@alu.edu.gva.es>
2025-02-06 17:36:59 +0100

    listado de ubicaciones funcional

--- Inventario/src/app/marca/components/lista/lista.component.ts
+++ Inventario/src/app/marca/components/lista/lista.component.ts
@@ -7,16 +7,15 @@ import { MarcasService } from '../../marcas.service';
   styleUrl: './lista.component.css'
 })
 export class ListaComponent {
-  public filterSearch:string = '';
+ // public filterSearch:string = '';
   public cargando:boolean = true;
   public marcas: any;
   constructor(private _marcasService: MarcasService) { }
   ngOnInit() {
     this._marcasService.obtengoMarcasApi().subscribe({
-      next: (resultado) => {
-        if (resultado.mensaje == "OK"){
-          this.marcas = resultado.datos;
-          console.log(resultado.datos);
+      next: (resultado) => {        
+        if (resultado){
+          this.marcas = resultado;
         }else{
           console.error('Error al recibir datos:', resultado.error);
         }
--- Inventario/src/app/ubicaciones/components/lista/lista.component.ts
+++ Inventario/src/app/ubicaciones/components/lista/lista.component.ts
@@ -4,20 +4,18 @@ import { UbicacionesService } from '../../ubicaciones.service';
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  standalone: false,
   styleUrl: './lista.component.css'
 })
 export class ListaComponent {
-  public filterSearch:string = '';
+ // public filterSearch:string = '';
   public cargando:boolean = true;
   public ubicaciones: any;
-  constructor(private _ubicacionesService: UbicacionesService) { }  
+  constructor(private _ubicacionesService: UbicacionesService) { }
   ngOnInit() {
     this._ubicacionesService.obtengoUbicacionesApi().subscribe({
-      next: (resultado) => {
-        if (resultado.mensaje == "OK"){
-          this.ubicaciones = resultado.datos;
-          console.log(resultado.datos);
+      next: (resultado) => {        
+        if (resultado){
+          this.ubicaciones = resultado;
         }else{
           console.error('Error al recibir datos:', resultado.error);
         }
