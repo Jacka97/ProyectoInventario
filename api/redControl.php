205922fc3694069fdd8846bcf02f80a5522789df
Desock <salvagomezrisen@gmail.com>
2025-02-10 20:16:26 +0100

    Añadido el controlador de dispositivos de red

---
+++ api/redControl.php
@@ -0,0 +1,84 @@
+<?php
+include '../modelo/consultaRed.php';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'), true);
+
+switch ($method) {
+    case 'GET':
+        header("Content-Type: application/json; charset=UTF-8");
+        if (isset($_GET['id'])) {
+
+            // Obtener un producto por ID.
+            $id = (int) $_GET['id']; // Sanitizar el ID.
+            $product = consultaDispoRed::getDispRedById($id);
+            echo json_encode($product ?: ["error" => "Producto no encontrado"]);
+        } else {
+
+            // Obtener todos los pedidos.
+            $products = consultaDispoRed::getAllDispRed();
+            echo json_encode($products);
+        }
+        break;
+
+    case 'POST':
+        header("Content-Type: application/json; charset=UTF-8");
+
+        if (!empty($input) &&  isset($input['nombre'], $input['instalado'], $input['ip'], $input['id_ubicacion'])) {
+            $nombre = $input['nombre'];
+            $instalado = $input['instalado'];
+            $ip = $input['ip'];
+            $id_ubicacion = $input['id_ubicacion'];
+
+            $result = consultaDispoRed::insertarDisRed($nombre, $instalado, $ip, $id_ubicacion);
+
+            if ($result) {
+                echo json_encode(["id" => $result]);
+            } else {
+                echo json_encode(["error" => "No se pudo crear el ordenador"]);
+            }
+        } else {
+            echo json_encode(["error" => "Datos inválidos o incompletos"]);
+        }
+        break;
+
+    case 'PUT':
+        header("Content-Type: application/json; charset=UTF-8");
+
+        if (!empty($input) &&  isset($input['nombre'], $input['instalado'], $input['ip'], $input['id_ubicacion'])) {
+            $nombre = $input['nombre'];
+            $instalado = $input['instalado'];
+            $ip = $input['ip'];
+            $id_ubicacion = (int) $input['id_ubicacion'];
+
+            $result = consultaDispoRed::insertarDisRed($nombre, $instalado, $ip, $id_ubicacion);
+
+            if ($result) {
+                echo json_encode(["success" => true]);
+            } else {
+                echo json_encode(["error" => "No se pudo actualizar el ordenador"]);
+            }
+        } else {
+            echo json_encode(["error" => "Datos inválidos o incompletos"]);
+        }
+        break;
+
+    case 'DELETE':
+        header("Content-Type: application/json; charset=UTF-8");
+        if (isset($_GET['id'])) {
+            // Eliminar un producto por ID.
+            $id = (int) $_GET['id']; // Sanitizar el ID.
+            $result = consultaDispoRed::eliminarDisRed($id);
+            echo json_encode(["success" => $result]);
+        } else {
+            echo json_encode(["error" => "ID no proporcionado"]);
+        }
+        break;
+
+    default:
+        echo json_encode(["error" => "Método no soportado"]);
+        break;
+}
