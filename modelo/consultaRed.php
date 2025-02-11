f07d063e916e623c1ab3ddb82568d6b6fc302862
Tony Abril <antabrrue@alu.edu.gva.es>
2025-02-11 16:48:01 +0100

    arreglo consultaRed

--- modelo/consultaRed.php
+++ modelo/consultaRed.php
@@ -41,14 +41,14 @@ class consultaDispoRed{
     }
     public static function actualizarDispositivoRed($id, $nombre, $idUbicacion, $idMarca, $modelo, $red, $macWifi, $ipWifi, $macLAN, $ipLAN, $tipoConexion, $tipoDisp, $observaciones, $precio){
         $conexion = conexionBD::conectar();
-        $sql = "UPDATE DispositivoRed SET nombre = '$nombre', idUbicacion = '$idUbicacion', idMarca = '$idMarca', modelo = '$modelo', Red = '$red', MACWIFI = '$macWifi', IPWIFI = '$ipWifi', MACLAN = '$macLAN', IPLAN = '$ipLAN', tipoConexion = '$tipoConexion', tipoDisp = '$tipoDisp', observable = '$observaciones', precio = '$precio' WHERE id = $id";
+        $sql = "UPDATE DispositivoRed SET nombre = '$nombre', idUbicacion = '$idUbicacion', idMarca = '$idMarca', modelo = '$modelo', Red = '$red', MACWIFI = '$macWifi', IPWIFI = '$ipWifi', MACLAN = '$macLAN', IPLAN = '$ipLAN', tipoConexion = '$tipoConexion', tipoDisp = '$tipoDisp', Observaciones = '$observaciones', precio = '$precio' WHERE id = $id";
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
     }
     public static function eliminarDisRed($id){
         $conexion = conexionBD::conectar();
-        $sql = "DELETE FROM DispositivoRed WHERE id_usu = $id";
+        $sql = "DELETE FROM DispositivoRed WHERE id = $id";
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
