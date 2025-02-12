a9fe930982069800f44e7d03ef02c40a97932c74
Tony Abril <antabrrue@alu.edu.gva.es>
2025-02-12 15:53:33 +0100

    lista de entradas y salidas hecha y servicio

---
+++ Inventario/src/app/material/components/salidas/salidas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-salidas',
+  templateUrl: './salidas.component.html',
+  styleUrl: './salidas.component.css'
+})
+export class SalidasComponent {
+
+}
