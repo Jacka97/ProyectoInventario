3ae3af797908f8e2f01c8ce1c0b9cff27148efa7
Desock <salvagomezrisen@gmail.com>
2025-02-13 15:58:46 +0100

    añadido el modelo de softwarePC

--- modelo/consultaSoftwarePC.php
+++ modelo/consultaSoftwarePC.php
@@ -2,44 +2,44 @@
 include "../modelo/dbConex.php";
 class consultaSoft{
     private $id;
+    private $idPC;
     private $nombre;
-    private $fecha;
   
-    public static function getAllSoftware(){
+    public static function getAllSoftwarePC(){
         $conexion = conexionBD::conectar();
-        $sql = "SELECT * FROM Software";
+        $sql = "SELECT * FROM Software_PC";
         $result = $conexion->query($sql);
         $conexion->close();
         return $result->fetch_all(MYSQLI_ASSOC);   
 }
 
-    public static function getSoftById($id){
+    public static function getSoftPCById($id){
         $conexion = conexionBD::conectar();
-        $sql = "SELECT * FROM Software WHERE id = " . $id;
+        $sql = "SELECT * FROM Software_PC WHERE id = " . $id;
         $result = $conexion->query($sql);
         $conexion->close();
         return $result->fetch_assoc();
     }
     
-    public static function insertarSoftware($nombre){
+    public static function insertarSoftwarePC($nombre){
         $conexion = conexionBD::conectar();
-        $sql = "INSERT INTO Software (nombre) VALUES ('$nombre')";
+        $sql = "INSERT INTO Software_PC (nombre) VALUES ('$nombre')";
         $conexion->query($sql);
         return $conexion->insert_id;
         $conexion->close();
         
     }
-    public static function modificarSoftw($id, $nombre){
+    public static function modificarSoftwPC($id, $nombre){
         $conexion = conexionBD::conectar();
-        $sql = "UPDATE Software SET nombre = '$nombre' WHERE id = " . $id;
+        $sql = "UPDATE Software_PC SET nombre = '$nombre' WHERE id = ".$id;
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
         
     }
-    public static function eliminarSoftware($id){
+    public static function eliminarSoftwarePC($id){
         $conexion = conexionBD::conectar();
-        $sql = "DELETE FROM Software WHERE id = $id";
+        $sql = "DELETE FROM Software_PC WHERE id = $id";
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
