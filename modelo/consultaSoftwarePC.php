70246fda124b90e2498fc9d2e204a97395fc5b2c
Desock <salvagomezrisen@gmail.com>
2025-02-13 17:54:59 +0100

    modelo de software pc hecho

--- modelo/consultaSoftwarePC.php
+++ modelo/consultaSoftwarePC.php
@@ -1,10 +1,7 @@
 <?php
 include "../modelo/dbConex.php";
 class consultaSoft{
-    private $id;
-    private $idPC;
-    private $nombre;
-  
+    
     public static function getAllSoftwarePC(){
         $conexion = conexionBD::conectar();
         $sql = "SELECT Software_PC.*, Software.nombre as softnombre, Ordenadores.nombre as pcnombre from Software_PC 
@@ -17,23 +14,25 @@ class consultaSoft{
 
     public static function getSoftPCById($id){
         $conexion = conexionBD::conectar();
-        $sql = "SELECT * FROM Software_PC WHERE id = " . $id;
+        $sql = "SELECT Software_PC.*, Software.nombre as softnombre, Ordenadores.nombre as pcnombre from Software_PC 
+            left join Software on Software.id = Software_PC.idSoftware 
+            left join Ordenadores on Ordenadores.id = Software_PC.idPC WHERE id = " . $id;
         $result = $conexion->query($sql);
         $conexion->close();
         return $result->fetch_assoc();
     }
     
-    public static function insertarSoftwarePC($nombre){
+    public static function insertarSoftwarePC($idSoft, $idPC){
         $conexion = conexionBD::conectar();
-        $sql = "INSERT INTO Software_PC (nombre) VALUES ('$nombre')";
+        $sql = "INSERT INTO Software_PC (idSoftware, idPC) VALUES ('$idSoft', '$idPC')";
         $conexion->query($sql);
         return $conexion->insert_id;
         $conexion->close();
         
     }
-    public static function modificarSoftwPC($id, $nombre){
+    public static function modificarSoftwPC($idSoft){
         $conexion = conexionBD::conectar();
-        $sql = "UPDATE Software_PC SET nombre = '$nombre' WHERE id = ".$id;
+        $sql = "UPDATE Software_PC SET idSoftware = '$idSoft' WHERE idSoftware = " . $idSoft;
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
