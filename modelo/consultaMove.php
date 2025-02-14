be748f468dbf49fd4e0f19d7293b14dea101a750
Tony Abril <antabrrue@alu.edu.gva.es>
2025-02-14 15:23:35 +0100

    modelo finalizado

--- modelo/consultaMove.php
+++ modelo/consultaMove.php
@@ -25,29 +25,6 @@ class consultaMove{
         $result = $conexion->query($sql); 
         $conexion->close();
         return $result->fetch_all(MYSQLI_ASSOC);
-
-        /*
-        evita inyeccion sql
-
-            $sql = "SELECT * FROM `Historico_Movimientos` WHERE `TipoMovimiento` = :tipo";
-            $params = [':tipo' => $tipo];
-
-            if (!is_null($fechamin)) {
-                $sql .= " AND `fecha` >= :fechamin";
-                $params[':fechamin'] = $fechamin;
-            }
-
-            if (!is_null($fechamax)) {
-                $sql .= " AND `fecha` < :fechamax";
-                $params[':fechamax'] = $fechamax;
-            }
-
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute($params);
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-        */
     }
 }
 ?>
