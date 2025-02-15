57657cabfb2e4dd444c71aa10267cb80f976a459
Desock <salvagomezrisen@gmail.com>
2025-02-15 18:20:02 +0100

    merge de mi local con el remoto

--- modelo/dbConex.php
+++ modelo/dbConex.php
@@ -3,12 +3,14 @@
 class conexionBD {
 
     private static $hostname = "127.0.0.1";
-    private static $database = "Inventario";
+    private static $database = "inventario";
     // private static $database = "inventario";
-    private static $user = "inventarios"; 
-    private static $password = "Q00dIr[8R*x";  
+    // private static $user = "inventarios"; 
+    // private static $password = "Q00dIr[8R*x";  
     // private static $user = "phpmyadmin";
     // private static $password = "1234";
+    private static $user = "root";
+    private static $password = "";
    
     public static function conectar(){
         try {
