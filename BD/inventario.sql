b93b3a3d6c9276da7c0a9758a1f17d1a19bb3f62
AnthonyAbril <antabrrue@alu.edu.gva.es>
2025-02-17 19:10:06 +0100

    arreglada consulta sql de incidencias

--- BD/inventario.sql
+++ BD/inventario.sql
@@ -519,7 +519,12 @@ CREATE TABLE Incidencia (
 ya que necesitamos una respuesta para mostrar al cliente de si la incidencia puede crearse o no,
 para ello lo que se hara es una consulta del tecnico con menos incidencias
 
-SELECT count(Incidencia.idTecnico), Usuarios.nombre FROM `Incidencia` left join Usuarios on Incidencia.idTecnico = Usuarios.id where Incidencia.estado != 'resuelto' and Incidencia.estado != 'cerrada'  group by Incidencia.idTecnico
+SELECT count(Incidencia.idTecnico) as cantidad_incidencias, Usuarios.nombre FROM `Incidencia` 
+left join Usuarios on Incidencia.idTecnico = Usuarios.id 
+WHERE Incidencia.estado != 'resuelto' and Incidencia.estado != 'cerrada' 
+group by Incidencia.idTecnico 
+ORDER BY cantidad_incidencias ASC 
+LIMIT 1
 
 De esta forma solo permitira crear incidencias si existe algun tecnico disponible (creado y activo)
 
