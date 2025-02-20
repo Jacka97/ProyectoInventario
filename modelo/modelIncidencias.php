74f49605e5da8306619e66bcfc9bfa329f4bc08d
Tony Abril <antabrrue@alu.edu.gva.es>
2025-02-20 13:59:58 +0100

    funcional

--- modelo/modelIncidencias.php
+++ modelo/modelIncidencias.php
@@ -6,7 +6,7 @@ class modelIncidencias{
 
     public static function getAllIncidencias(){
         $conexion = conexionBD::conectar();
-        $sql = "SELECT incidencia.id, incidencia.idTecnico, incidencia.idUbicacion, incidencia.asunto, incidencia.descripcion, incidencia.estado, incidencia.fechaCreacion, incidencia.fechaCierre,ubicaciones.nombre as nombreUbicacion FROM incidencia Inner join ubicaciones on incidencia.idUbicacion = ubicaciones.id;";
+        $sql = "SELECT Incidencia.id, Incidencia.idTecnico, Incidencia.idUbicacion, Incidencia.asunto, Incidencia.descripcion, Incidencia.estado, Incidencia.fechaCreacion, Incidencia.fechaCierre,Ubicaciones.nombre as nombreUbicacion FROM Incidencia Inner join Ubicaciones on Incidencia.idUbicacion = Ubicaciones.id;";
         $result = $conexion->query($sql);
         $conexion->close();
         return $result->fetch_all(MYSQLI_ASSOC);   
@@ -14,14 +14,14 @@ class modelIncidencias{
 
     public static function getIncidenciaByID($id){
         $conexion = conexionBD::conectar();
-        $sql = "SELECT incidencia.id, incidencia.idTecnico, incidencia.idUbicacion, incidencia.asunto, incidencia.descripcion, incidencia.estado, incidencia.fechaCreacion, incidencia.fechaCierre,ubicaciones.nombre as nombreUbicacion FROM incidencia Inner join ubicaciones on incidencia.idUbicacion = ubicaciones.id WHERE incidencia.id = " . $id;
+        $sql = "SELECT Incidencia.id, Incidencia.idTecnico, Incidencia.idUbicacion, Incidencia.asunto, Incidencia.descripcion, Incidencia.estado, Incidencia.fechaCreacion, Incidencia.fechaCierre,Ubicaciones.nombre as nombreUbicacion FROM Incidencia Inner join Ubicaciones on Incidencia.idUbicacion = Ubicaciones.id WHERE Incidencia.id = " . $id;
         $result = $conexion->query($sql);
         $conexion->close();
         return $result->fetch_assoc();
     }
     public static function altaIncidencia( $idTecnico, $idUbicacion, $asunto, $descripcion, $estado){
         $conexion = conexionBD::conectar();
-        $sql = "INSERT INTO incidencia(idTecnico, idUbicacion, asunto, descripcion, estado) VALUES ('$idTecnico','$idUbicacion','$asunto','$descripcion','$estado')";
+        $sql = "INSERT INTO Incidencia(idTecnico, idUbicacion, asunto, descripcion, estado) VALUES ('$idTecnico','$idUbicacion','$asunto','$descripcion','$estado')";
         $conexion->query($sql);
         return $conexion->insert_id;
         $conexion->close();
@@ -29,7 +29,7 @@ class modelIncidencias{
     }
     public static function modificarIncidencia($idTecnico, $idUbicacion, $asunto, $descripcion, $estado, $EmailEnviado){
         $conexion = conexionBD::conectar();
-        $sql = "UPDATE incidencia SET idTecnico = '$idTecnico', idUbicacion = '$idUbicacion', asunto = '$asunto', descripcion = '$descripcion', estado = '$estado'";
+        $sql = "UPDATE Incidencia SET idTecnico = '$idTecnico', idUbicacion = '$idUbicacion', asunto = '$asunto', descripcion = '$descripcion', estado = '$estado'";
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
@@ -37,7 +37,7 @@ class modelIncidencias{
     }
     public static function actualizarEstado($id, $estado){
         $conexion = conexionBD::conectar();
-        $sql = "UPDATE incidencia SET estado = '$estado' WHERE id = $id";
+        $sql = "UPDATE Incidencia SET estado = '$estado' WHERE id = $id";
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
@@ -47,7 +47,7 @@ class modelIncidencias{
    
     public static function eliminarIncidencia($id){
         $conexion = conexionBD::conectar();
-        $sql = "DELETE FROM incidencia WHERE id = " . $id;
+        $sql = "DELETE FROM Incidencia WHERE id = " . $id;
         $conexion->query($sql);
         return $conexion->affected_rows;
         $conexion->close();
@@ -55,12 +55,12 @@ class modelIncidencias{
     }
 
 
-/*Voy a obtener el técnico que tenga menos incidencias en estado cerrada para asignarle la nueva incidencia que se cree*/
+/*Voy a obtener el técnico que tenga menos Incidencias en estado cerrada para asignarle la nueva Incidencia que se cree*/
     public static function getTecnicoMenosIncidencias(){
     $conexion = conexionBD::conectar();
     $sql = "SELECT u.id AS idTecnico, u.correo AS correo, u.nombre, COUNT(i.id) AS cantidad_incidencias
     FROM usuarios u
-    LEFT JOIN incidencia i ON u.id = i.idTecnico AND i.estado != 'cerrado'
+    LEFT JOIN Incidencia i ON u.id = i.idTecnico AND i.estado != 'cerrado'
     WHERE u.id_rol = 3 AND u.activo = 1
     GROUP BY u.id
     ORDER BY cantidad_incidencias ASC, u.id ASC
